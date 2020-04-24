b38dcb8afced95c61442894a53d57cf2715c693c
pavTiger <pavTiger@gmail.com>
2020-04-24 10:41:55 +0300

    O_o

---
+++ ser_selenium.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.insert(0,'/usr/lib/chromium-browser/chromedriver')
+from selenium import webdriver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+wd = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
+wd.get("https://www.webite-url.com")
